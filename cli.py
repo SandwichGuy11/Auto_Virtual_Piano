d2848fbf1227b41627b65e0df46279bdffd8245c
SandwichGuy11 <Sandwichguy212@gmail.com>
2025-03-20 22:13:47 +0800

    Added main loop prototype and shifted key checker

--- cli.py
+++ cli.py
@@ -1,29 +1,56 @@
 
 import functions
 import glob
+import keyboard
 from pathlib import Path
 
 # 1: show list of sheets to pick from and have user pick 1
 # 2: parse the sheets so the program can tell singular notes from grouped notes
 # 3: listen for given key presses and play a note on keypress
 # 4: do step 3 until no more notes left
+# 5: add a key to quit the program when pressed at any point during loop
 
 
-sheet_paths = glob.glob("sheets/*")
-sheets_list = [Path(filepath).stem for filepath in sheet_paths]
+PLAY_NOTE_1 = ","
+PLAY_NOTE_2 = "."
+
+SHEET_PATHS = glob.glob("sheets/*")
+sheets_list = [Path(path).stem for path in SHEET_PATHS]
+sheet = None
 
 # sheet_dir = Path("sheets").resolve()
 # print(f"Sheets Directory: {sheet_dir}")
-print("---------- SHEETS LIST ----------")
-for index, title in enumerate(sheets_list):
-    print(f"{index + 1}. {title}")
 
-try:
-    sheet_num = int(input("\nPick a sheet. Enter a number: "))
-    sheet_num -= 1
-    sheet_dir = sheet_paths[sheet_num]
-    sheet = functions.parse_sheet(sheet_dir)
-    print(sheet)
+is_choosing = True
+while is_choosing:
+    try:
+        # Display available sheets
+        print("---------- SHEETS LIST ----------")
+        for index, title in enumerate(sheets_list):
+            print(f"{index + 1}. {title}")
 
-except ValueError:
-    print("Please enter a number.")
+        user_input = input("\nEnter a number or type 'exit' to quit: ")
+        if user_input.upper() == "EXIT":
+            print("Quitting program..")
+            is_choosing = False
+        else:
+            sheet_num = int(user_input) - 1
+            sheet_dir = SHEET_PATHS[sheet_num]
+            sheet_name = sheets_list[sheet_num]
+            sheet = functions.parse_sheet(sheet_dir)
+            print(f"Selected: {sheet_name}")
+            is_choosing = False
+
+    except ValueError:
+        print("Enter a valid number.")
+
+
+if sheet:
+    while len(sheet) > 0:
+        # this is just a placeholder for the event
+        inp = input("Enter g: ")
+        if inp == "g":
+            print(sheet[0])
+            sheet.pop(0)
+
+    print("No more notes left.")
--- functions.py
+++ functions.py
@@ -30,10 +30,50 @@ def parse_sheet(sheet_dir: str):
         print(f"{e}: The specified file was not found.")
 
 
+def is_note_group(note: str):
+     # TODO: check if the str is encased in [].
+
+
+def is_shifted(char: str):
+    """Checks if a character requires the Shift key to be pressed to be typed with a keyboard.
+
+    It checks if the given character is an uppercase letter or a special
+    character that is accessed by holding down the Shift key.
+
+    :param char: A single character (length of 1).
+    :return: bool: True if the character requires the Shift key, False otherwise.
+    """
+    ascii_value = ord(char)  # get integer that represents char i.e (a = 97)
+    if 65 <= ascii_value <= 90:  # 65-90 represents uppercase letters
+        return True
+    if char in "!@#$%^&*()_+{}|:\"<>?":
+        return True
+    return False
+
+
+def play_note(note: str):
+    # TODO: play the note if single char, else if note is encased in [], iterate through and play all notes.
+    conversion_cases = {'!': '1',
+                        '@': '2',
+                        '£': '3',
+                        '$': '4',
+                        '%': '5',
+                        '^': '6',
+                        '&': '7',
+                        '*': '8',
+                        '(': '9',
+                        ')': '0'}
+
+    if is_shifted(note):
+        if note in conversion_cases:
+            note = conversion_cases[note]  # convert to symbol to number
+
+
+
 if __name__ == "__main__":
     import time
 
-    print(parse_sheet("sheets/Stromae - Alors On Danse.txt"))
-    for i in parse_sheet("sheets/Stromae - Alors On Danse.txt"):
+    print(parse_sheet("sheets/DS3 - Sister Friede And Father Ariandel.txt"))
+    for i in parse_sheet("sheets/DS3 - Sister Friede And Father Ariandel.txt"):
         print(i)
         # time.sleep(0.7)
